39bf75b878fcd8bb6399ea9f732f6aaae82ff862
Ajaymanath6 <Ajaymanath6@users.noreply.github.com>
2026-01-20 11:41:16 +0530

    Clean up: Remove all pages except AiShoppingPage, set as default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
-import LandingPage from './pages/LandingPage'
-import ScanningPage from './pages/ScanningPage'
-import HealthReportPage from './pages/HealthReportPage'
-import FixPage from './pages/FixPage'
-import FixPreviewPage from './pages/FixPreviewPage'
-import ConfirmationPage from './pages/ConfirmationPage'
 import AiShoppingPage from './pages/AiShoppingPage'
 
 function App() {
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/diagnostics/scanning" element={
-          <MainLayout>
-            <div className="max-w-4xl mx-auto my-10 px-4">
-              <ScanningPage />
-            </div>
-          </MainLayout>
-        } />
-        <Route path="/diagnostics/report" element={
-          <MainLayout>
-            <div className="max-w-4xl mx-auto my-10 px-4">
-              <HealthReportPage />
-            </div>
-          </MainLayout>
-        } />
-        <Route path="/diagnostics/fix-preview" element={
-          <MainLayout>
-            <div className="max-w-4xl mx-auto my-10 px-4">
-              <FixPreviewPage />
-            </div>
-          </MainLayout>
-        } />
-        <Route path="/diagnostics/fix/:issueId" element={
-          <MainLayout>
-            <div className="max-w-4xl mx-auto my-10 px-4">
-              <FixPage />
-            </div>
-          </MainLayout>
-        } />
-        <Route path="/diagnostics/confirmed" element={
-          <MainLayout>
-            <div className="max-w-4xl mx-auto my-10 px-4">
-              <ConfirmationPage />
-            </div>
-          </MainLayout>
-        } />
-        <Route path="/ai-shopping" element={<AiShoppingPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<AiShoppingPage />} />
+    </Routes>
   )
 }
 
